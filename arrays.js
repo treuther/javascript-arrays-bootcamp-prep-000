2def1ef21f217a8eb91d85a97276f0992eb1308f
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 01:06:59 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,41 @@
+
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+ function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element)
+}
+
+function addElementToEndOfArray(array, element) {
+  return [element, ...array];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element)
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
+  return array
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop()
+}
+
+function removeElementFromEndOfArray() {
+  return array.slice(0, array.length - 1)
+}
+
+function
