2596419809f2326fb0dfb90fd863e884004e793c
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 01:28:04 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -34,6 +34,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array.pop()
 }
 
-function removeElementFromEndOfArray() {
+function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
+
