ad52f2ee5034093c9b387c9f989ed8c22d5969cb
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 01:32:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  return [element, ...array];
+  return [...array, element];
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
